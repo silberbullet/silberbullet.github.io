c7abaa5ec245269b58e8b81bb0bc1ea754575935
kwengwoo <pkw19961027@gmail.com>
2024-05-23 18:56:43 +0900

    많이 발전햇다 그래도

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react'
-
-// Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/pagination'
 import { Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import Introduce from './Introduce'
 
 const AboutMe = () => {
   return (
@@ -19,7 +17,9 @@ const AboutMe = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>
+          <Introduce />
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
